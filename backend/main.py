5d26b64a94a1cc2fdad896f8d4747129b5cbaf40
Ashwin8558 <ashwin@orchestro.ai>
2026-02-22 21:56:18 +0530

    First version of App

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI(title="Knowledge Vault API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000","https://digivault-mauve.vercel.app"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000","https://digi-vault.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
